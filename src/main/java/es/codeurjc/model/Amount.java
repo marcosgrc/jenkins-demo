9d28324185aa76ad39c33fb152a333c150da58c0
marcosgrc <m.garciaga.2022@alumnos.urjc.es>
2026-04-19 14:44:57 +0200

    Added: E2E own accounts transfer test, selenium dependencies and set up TrasferE2ETest test class

--- src/main/java/es/codeurjc/model/Amount.java
+++ src/main/java/es/codeurjc/model/Amount.java
@@ -1,8 +1,9 @@
 package es.codeurjc.model;
 
 /**
- * Clase que representa un monto de dinero 
- * Soluciona el Bad Smell de "Primitive Obsession" al evitar el uso de tipos primitivos (double)
+ * Clase que representa un monto de dinero
+ * Soluciona el Bad Smell de "Primitive Obsession" al evitar el uso de tipos
+ * primitivos (double)
  * para representar conceptos con reglas de negocio específicas.
  */
 public class Amount {
@@ -12,7 +13,9 @@ public class Amount {
     /**
      * Constructor que valida las reglas de negocio de la aplicación.
      * * @param value El valor numérico del monto.
-     * @throws IllegalArgumentException si el monto es negativo o supera los 20.000€.
+     * 
+     * @throws IllegalArgumentException si el monto es negativo o supera los
+     *                                  20.000€.
      */
 
     public Amount(double value) {
@@ -29,6 +32,10 @@ public class Amount {
         }
 
         this.value = value;
+    }
+
+    public Amount(String value) {
+        this(Double.parseDouble(value));
     }
 
     public double getValue() {
